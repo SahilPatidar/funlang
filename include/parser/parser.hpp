88e0bb9cecbc70401c70599f3fe02e05cf21c819
SahilPatidar <patidarsahil@2001gmail.com>
2022-10-31 11:32:04 +0530

    Ast.h

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -9,13 +9,17 @@ namespace parser{
         lex::Token_type cur_token;
         lex::tok_t toks;
         int cur_index = 0;
-    //    AstPtr NodePtr;
         std::string path;
         std::string file_name;
-
         void next();
         void advance();
         AstPtr parseBlockStatement();
+        AstPtr parseIdentifier();
+        AstPtr parseFor();
+        AstPtr parseWhile();
+        AstPtr parseEnum();
+        AstPtr parseStruct();
+        AstPtr parseTypeCast();
         AstPtr parseExpression();
         AstPtr IfStatement();
         AstPtr parseProgram();
@@ -24,7 +28,6 @@ namespace parser{
         :toks(_toks), path(_path), file_name(_file_name) {}
         ~Parser();
         AstPtr parse();
-
     };
 
 }
